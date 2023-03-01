e3c36238ba725ba8a9a63a1cb31850d7ed197858
ElenaPrisacari <lenaprisacari111@gmail.com>
2023-03-01 21:39:47 +0200

    case

---
+++ 13-02-23/Task 1/app.js
@@ -0,0 +1,36 @@
+let number = prompt('Number?', '');
+switch (number) {
+    case '0':
+        alert( 'Zero' );
+    break; 
+    case '1':
+  alert( 'One' );
+  break; 
+  case '2':
+  alert( 'Two' );
+  break; 
+  case '3':
+  alert( 'Three' );
+  break; 
+  case '4':
+  alert( 'Four' );
+  break; 
+    case '5':
+    alert( 'Five' );
+    break; 
+    case '6':
+    alert( 'Six' );
+    break; 
+    case '7':
+    alert( 'Seven' );
+    break; 
+    case '8':
+      alert( 'Eight' );
+      break; 
+      case '9':
+        alert( 'Nine' );
+        break; 
+        default:
+    alert  (number + ' is not a digit');}
+
+
