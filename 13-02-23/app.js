000ac5d85cea56a488dff69714284b3c40f7aef5
ElenaPrisacari <lenaprisacari111@gmail.com>
2023-03-01 20:39:35 +0200

    final task 1

--- 13-02-23/app.js
+++ 13-02-23/app.js
@@ -1,3 +1,4 @@
+
 // Task #1 - array display
 // 3.4.1(a)
 
@@ -6,3 +7,33 @@ const values = number;
 for (i = 0; i < values.length; i++) {
     console.log(values[i]);
 }
+
+//  3.4.1(b)
+
+const number = [1, 2, 3, 4, 5];
+for (let list in number) {
+    console.log(number[list]);
+}
+
+//  3.4.1(c)
+
+const number = [1, 2, 3, 4, 5];
+for (const list of number) {
+    console.log(list);
+}
+
+//  3.4.1(d)
+
+const number = [1, 2, 3, 4, 5];
+number.forEach(element => console.log(element));
+
+//  3.4.1(e)
+
+const number = [1, 2, 3, 4, 5];
+const mapFunction = number.map(x => x + 0);
+console.log(mapFunction);
+
+// 3.4.1(f)
+
+const number = [1, 2, 3, 4, 5];
+console.log(number);
